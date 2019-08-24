1ed4c61a373ef34d941be38319a20f5042198adc
Sander Buruma <sanderburuma@gmail.com>
2019-08-24 18:02:24 +0200

    015 and 016 finished

---
+++ 015 Lattice Paths/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_Lattice_Paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type in how big the lattice grid is for which you want to calculate the total number of unique routes: ");
+
+            long calcLen = long.Parse(Console.ReadLine()) + 1;
+            List<long>[] pascalsPyramid = new List<long>[calcLen];
+            pascalsPyramid[0] = new List<long> { 1 };
+            pascalsPyramid[1] = new List<long> { 1, 1 };
+            for (long i = 2; i < calcLen; i++)
+            {
+                pascalsPyramid[i] = new List<long> {1};
+                for (long j = 1; j < i; j++)
+                {
+                    pascalsPyramid[i].Add(pascalsPyramid[i - 1][(int)j-1] + pascalsPyramid[i - 1][(int)j]);
+                }
+                pascalsPyramid[i].Add(1);
+            }
+
+            long sum = 0;
+            foreach (long pp in pascalsPyramid[calcLen - 1])
+            {
+                sum += pp * pp;
+            }
+            Console.WriteLine(sum);
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 016 Power Digit Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_Power_Digit_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger finalnumber = 1;
+            int start = (int)Math.Pow(2, 25);
+            for (int i = 0; i < 40; i++)
+            {
+                finalnumber *= start;
+            }
+            int sum = 0;
+            foreach (char c in finalnumber.ToString().ToCharArray())
+            {
+                sum += c - 48;
+            }
+            Console.WriteLine(sum.ToString("N0"));
+            Console.ReadKey();
+        }
+    }
+}
