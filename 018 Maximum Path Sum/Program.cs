eabb92ce6b957c4e4238f24571dbc6ab3a0377bf
Sander Buruma <sanderburuma@gmail.com>
2019-08-25 00:07:05 +0200

    018 solved

--- 018 Maximum Path Sum/Program.cs
+++ 018 Maximum Path Sum/Program.cs
@@ -8,8 +8,33 @@ namespace _018_Maximum_Path_Sum
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<List<int>> pyramid = new List<List<int>> { };
+            pyramid.Add(new List<int> { 75, });
+            pyramid.Add(new List<int> { 95, 64, });
+            pyramid.Add(new List<int> { 17, 47, 82, });
+            pyramid.Add(new List<int> { 18, 35, 87, 10, });
+            pyramid.Add(new List<int> { 20, 04, 82, 47, 65, });
+            pyramid.Add(new List<int> { 19, 01, 23, 75, 03, 34, });
+            pyramid.Add(new List<int> { 88, 02, 77, 73, 07, 63, 67, });
+            pyramid.Add(new List<int> { 99, 65, 04, 28, 06, 16, 70, 92, });
+            pyramid.Add(new List<int> { 41, 41, 26, 56, 83, 40, 80, 70, 33, });
+            pyramid.Add(new List<int> { 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, });
+            pyramid.Add(new List<int> { 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, });
+            pyramid.Add(new List<int> { 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, });
+            pyramid.Add(new List<int> { 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, });
+            pyramid.Add(new List<int> { 63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, });
+            pyramid.Add(new List<int> { 04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23 });
+
+            for (int i = pyramid.Count-1; i > 0; i--)
+            {
+                for (int j = 0; j < pyramid[i].Count - 1; j++)
+                {
+                    pyramid[i - 1][j] += Math.Max(pyramid[i][j], pyramid[i][j+1]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
